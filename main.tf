5ebd1baad3fdf0c196b0e962a5acf1ed427f2b7f
Saikiran243275 <85923393+Saikiran243275@users.noreply.github.com>
2021-10-25 16:03:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 provider "azurerm" {
 
     features {}
-  subscription_id = "a2ad4c5e-917a-40c0-a1b9-da92bd99e74f"
-  client_id       = "0d32c66c-e0be-4a5f-b0d7-8cb39869219a"
-  client_secret   = "3n67Q~bPdUsuGKAZzHBF2v3I6GIDE0l1MZHk~"
-  tenant_id       = "415a8c7e-8647-4b46-b291-9cd6c3cde41d"
+  subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_secret   = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 resource "azurerm_resource_group" "main" {
