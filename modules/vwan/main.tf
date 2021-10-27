aa0f63aeb4bd513121788a2873c70f7adef351c3
SK2021ht66106 <92103539+SK2021ht66106@users.noreply.github.com>
2021-10-27 15:48:33 +0530

    Update main.tf

--- modules/vwan/main.tf
+++ modules/vwan/main.tf
@@ -22,3 +22,12 @@ resource "azurerm_virtual_hub_connection" "main" {
   virtual_hub_id            = azurerm_virtual_hub.main[each.value.region].id
   remote_virtual_network_id = each.value.id
 }
+  
+resource "azurerm_express_route_gateway" "main" {
+  for_each            = { for e in var.er_gateway : e.name => e }
+  name                = each.value.name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  virtual_hub_id      = azurerm_virtual_hub.main[each.value.region].id
+  scale_units         = each.value.scale_units
+}
